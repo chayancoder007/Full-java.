fc92a3b22f8fe2c2beff623aa86bb3399061d476
The Chayan <91484285+chayancoder007@users.noreply.github.com>
2023-05-08 21:23:40 +0530

    Add files via upload

---
+++ addstring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+import javax.swing.text.html.StyleSheet;
+public class addstring {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        String firstname= sc.nextLine();
+        String lastname= sc.nextLine();
+
+                System.out.println(firstname+" "+lastname);    /*  here instead for writing two sout statement we can add two 
+                                                                 string value in one by just using " + "*/
+
+
+    }
+    
+}
